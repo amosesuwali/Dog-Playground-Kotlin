b8c0e08c8e7935f93ef12c75e80e7f85831d86a9
Amose Suwali <amose.suwali@gmail.com>
2022-01-11 22:53:14 +0200

    Created Custom Binding Adapter For the Dog Category

--- app/src/main/java/co/zw/amosesuwali/dogplayground/data/BindingAdapters.kt
+++ app/src/main/java/co/zw/amosesuwali/dogplayground/data/BindingAdapters.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import co.zw.amosesuwali.dogplayground.R
+import co.zw.amosesuwali.dogplayground.models.BreedDetailModel
+import co.zw.amosesuwali.dogplayground.models.BreedRandomResponse
 import co.zw.amosesuwali.dogplayground.models.DogCeoApiStatus
 import co.zw.amosesuwali.dogplayground.models.Pictures
 import coil.load
@@ -19,6 +21,12 @@ import coil.load
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<String>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
 
+    adapter.submitList(data)
+}
+
+@BindingAdapter("breedListData")
+fun bindBreedRecyclerView(recyclerView: RecyclerView, data: List<BreedDetailModel>?) {
+    val adapter = recyclerView.adapter as BreedListAdapter
     adapter.submitList(data)
 }
 
@@ -37,6 +45,17 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
     }
 }
 
+@BindingAdapter("breedImageUrl")
+fun bindBreedImage(imgView: ImageView, imgUrl: BreedRandomResponse?) {
+    imgUrl?.let {
+
+        val imgUri = imgUrl.message.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
+    }
+}
 /**
  * This binding adapter displays the [DogCeoApiStatus] of the network request in an image view.  When
  * the request is loading, it displays a loading_animation.  If the request has an error, it
