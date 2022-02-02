e019c7af2d27585092c5e76acd2165585671d239
Amose Suwali <amose.suwali@gmail.com>
2022-02-02 17:07:51 +0200

    Added Dashboard Fragment

---
+++ app/src/main/java/co/zw/amosesuwali/dogplayground/Dashboard.kt
@@ -0,0 +1,31 @@
+package co.zw.amosesuwali.dogplayground
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Dashboard : Fragment() {
+
+    companion object {
+        fun newInstance() = Dashboard()
+    }
+
+    private lateinit var viewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/co/zw/amosesuwali/dogplayground/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package co.zw.amosesuwali.dogplayground
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
