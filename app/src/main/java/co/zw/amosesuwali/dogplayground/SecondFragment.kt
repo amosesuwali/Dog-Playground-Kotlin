9966704b651f169019ac020a1b192679acfe7d74
Amose Suwali <amose.suwali@gmail.com>
2022-02-06 18:39:22 +0200

    Update SecondFragment.kt

--- app/src/main/java/co/zw/amosesuwali/dogplayground/SecondFragment.kt
+++ app/src/main/java/co/zw/amosesuwali/dogplayground/SecondFragment.kt
@@ -1,11 +1,10 @@
 package co.zw.amosesuwali.dogplayground
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import co.zw.amosesuwali.dogplayground.databinding.FragmentSecondBinding
 
 /**
